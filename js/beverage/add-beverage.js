de86f76cf07323f02462ffb094481e068c08cdfa
Cristopher Molina <58301825+Csmolina@users.noreply.github.com>
2024-04-09 09:42:55 -0500

    feat: add beverage pages for show, add and update with js folder and files with endpoints connection

---
+++ js/beverage/add-beverage.js
@@ -0,0 +1,38 @@
+function postBeverage(ingredient) {
+
+  fetch('http://127.0.0.1:5000/beverage/', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+      },
+  })
+      .then(res => res.json())
+      .then(res => showNotification());
+
+
+}
+
+let beverageForm = $("#beverage-form");
+beverageForm.submit(event => {
+
+  let beverage = getBeverageData();
+  postBeverage(beverage);
+
+  event.preventDefault();
+  event.currentTarget.reset();
+});
+
+function getBeverageData() {
+
+  return {
+      name: $("input[name='name']").val(),
+      price: $("input[name='price']").val(),
+  };
+}
+
+function showNotification() {
+  let beverageAlert = $("#beverage-alert");
+  beverageAlert.toggle();
+  setTimeout(() => beverageAlert.toggle(), 5000);
+}
